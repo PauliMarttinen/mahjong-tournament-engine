1dee19776479ef65413c9381fd9a64ae007fb96b
Pauli Marttinen <48649834+PauliMarttinen@users.noreply.github.com>
2025-10-31 23:08:40 +0200

    Add premade seating templates

---
+++ src/views/NewTournament/PlayerEntry/premadeSeatingTemplates/premadeSeatingTemplates.ts
@@ -0,0 +1,19 @@
+import r8p16 from "./r8p16.json";
+import r8p24 from "./r8p24.json";
+import r8p28 from "./r8p28.json";
+import r8p32 from "./r8p32.json";
+import r8p36 from "./r8p36.json";
+import r8p40 from "./r8p40.json";
+import r8p44 from "./r8p44.json";
+import r8p48 from "./r8p48.json";
+
+export const premadeSeatingTemplates: { [key: string]: number[][] } = {
+	"r8p16": r8p16,
+	"r8p24": r8p24,
+	"r8p28": r8p28,
+	"r8p32": r8p32,
+	"r8p36": r8p36,
+	"r8p40": r8p40,
+	"r8p44": r8p44,
+	"r8p48": r8p48,
+};
