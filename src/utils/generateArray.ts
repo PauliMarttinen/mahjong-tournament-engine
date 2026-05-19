32a4b91e84320adee264dbeb0a02cb0dcc39c4a7
Pauli Marttinen <48649834+PauliMarttinen@users.noreply.github.com>
2026-05-19 23:22:24 +0300

    improve optional parameter implementation

--- src/utils/generateArray.ts
+++ src/utils/generateArray.ts
@@ -1,6 +1,3 @@
-export const generateArray = (itemCount: number, startFrom?: number): number[] => {
-	return Array(itemCount).fill(0).map((_: number, i: number): number => {
-		if (startFrom) return i + startFrom;
-		return i;
-	});
+export const generateArray = (itemCount: number, startFrom: number = 0): number[] => {
+	return Array(itemCount).fill(0).map((_: number, i: number): number => i + startFrom);
 };
